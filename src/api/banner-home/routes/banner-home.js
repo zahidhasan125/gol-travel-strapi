9b895349b889499411ee0a95905c8b97cf1df86e
zahidhasan125 <zahidhasan125@gmail.com>
2023-03-03 03:55:32 +0600

    postgres installed and configured

---
+++ src/api/banner-home/routes/banner-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * banner-home router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::banner-home.banner-home');
---
+++ src/api/banner-home/services/banner-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * banner-home service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::banner-home.banner-home');
